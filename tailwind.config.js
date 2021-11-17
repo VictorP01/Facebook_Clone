83a4c144fcdfb7590c5eced0281e479c5de4e595
Victor Paulo da Silva Arruda <victor0070000@gmail.com>
2021-11-16 22:31:09 -0300

    Create extensions for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        30: "7.5rem",
+        "290px": "290px",
+        "396px": "396px",
+        "500px": "500px",
+        "980px": "980px",
+      },
+      fontSize: { subtitle: "1.7rem" },
+      colors: {
+        "main-gray": "#f0f2f5",
+        "main-blue": "#166fe5",
+        "main-blue-hover": "#1877f2",
+        "main-green": "#36a420",
+        "main-green-hover": "#42872a",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
